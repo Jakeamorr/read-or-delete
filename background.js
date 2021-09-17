2f211c95f09f6f1a79bc3e25d644e5159f9d473c
Jake Morris <Jakeamorr@gmail.com>
2021-09-17 14:37:05 -0400

    test chrome storage api

--- background.js
+++ background.js
@@ -4,5 +4,24 @@ chrome.runtime.onInstalled.addListener(async () => {
   let url = chrome.runtime.getURL("collection.html");
   let tab = await chrome.tabs.create({ url });
 
+  chrome.storage.sync.set({ articles: ["a", "b"] });
+  chrome.storage.sync.get(["articles"], (res) => {
+    console.log("Articles value is: " + res.articles);
+  });
+
+  chrome.storage.sync.set({ articles: ["a", "b", "c"] });
+
+  chrome.storage.sync.get(["articles"], (res) => {
+    console.log("Articles value is: " + res.articles);
+  });
+
   console.log(`Created new tab ${tab.id}`);
 });
+
+// chrome.action.onClicked.addListener(async (tab) => {
+//   tab.url;
+//   let articles = chrome.storage.sync.get("articles");
+//   articles.push(tab.url);
+//   chrome.storage.sync.set({ articles });
+//   console.log(articles);
+// });
